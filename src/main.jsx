8395f48afa502a8eaa4e577edee3bfaf7e7a7e89
Chiragsinghal24 <chiragsinghal2406@gmail.com>
2023-07-04 16:27:17 +0530

    Fixed type

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Auth from './components/auth.jsx'
+import Auth from './components/Auth.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
